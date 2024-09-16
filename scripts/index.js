7b4c0b2877a821a4da2efbf6c406466f5495d6f8
d.zverev <d.zverev@yadro.com>
2024-09-16 19:51:09 +0300

    First commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const places = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
+function addCardTemplate(location, img = '', callback = '') {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src += img
+    cardElement.querySelector('.card__title').textContent = location;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', callback)
+    places.append(cardElement)
+}
 
 // @todo: Функция удаления карточки
+function removeCard(event) {
+    event.target.parentElement.remove()
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => addCardTemplate(item.name, item.link, removeCard))
